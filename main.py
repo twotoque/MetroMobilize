bfe5432a50025cf9bf1c866beb9acce191b94d49
twotoque <dereksong28@gmail.com>
2024-05-04 19:07:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ import json
 
 intents = discord.Intents.default()
 intents.members = True 
+intents.guilds = True
 intents.message_content = True
+intents.dm_messages = True 
 prefix = "up!"
 bot = commands.Bot(command_prefix = prefix, help_command = None, intents = intents)
 
@@ -20,14 +22,25 @@ async def on_ready():
     print("Ready")
 
 @bot.command()  
-async def hello(ctx):  
-    print("Ready")
+async def hello(ctx, title, description, role): 
+     
+    embed = discord.Embed(
+        title = title,                
+        description= (f"{description} \n This event is tailored for {role}"),  
+        color=discord.Color.blue()           
+    )
     
     button1 = Button(label = "Click me!", style = discord.ButtonStyle.green)
     button2 = Button(label = "Click me!", style = discord.ButtonStyle.red)
     view = View()
     view.add_item(button1)
     view.add_item(button2)
-    await ctx.send("Hello, world!", view = view)  
+
+    role = discord.utils.get(ctx.guild.roles, name=role)
+    for member in ctx.guild.members:
+        if role in member.roles:
+            await member.send("New event! \n You are recieving this message because you opted-in for events relating to this area. To stop recieving event information, click the appropriate button", embed=embed, view = view)  
+
+    await ctx.send(embed=embed, view = view)  
 
 bot.run(token)
